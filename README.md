a07699c2d1b9818061b32e28c2520bddbd239886
Picavet Noa <noa.picavet@leerling.gent.be>
2025-01-22 10:44:24 +0100

    Completed the genertor and updated the index and README file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# :point right: What is this?
-###### This is a random compliment generator wich will brighten your day!
+# :point_right:s So, What's this?!
+###### This is a random compliment generator and you will feel good after using this!
--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+//fetchCompliments
+async function fetchCompliments() {
+    try {
+        const response = await fetch('./data/complimenten.json');
+        console.log(response);
+        if (!response.ok) {
+            throw new Error("Could not load the JSON file, check inspector!");
+             }
+             const data = await response.json();
+             console.log(data);
+             return data.complimenten;
+     } catch (error) {
+        console.error("Could not obtain the compliments!");
+        return ["Je bent geweldig!","Blijf stralen!","Optimist!"]
+    } finally {
+      console.log("fetchcCompliments is done.")
+    };
+};
+// display compliments
+function displayRandomCompliment(compliments){
+    const complimentElement = document.getElementById('compliment');
+    const RandomCompliment = compliments[Math.floor(Math.random()* compliments.length)];
+    complimentElement.textContent = RandomCompliment;
+}
 // main function IIFE 
-((async)=>{
+(async ()=>{
  const compliments = await fetchCompliments();
+ const button = document.getElementById('generateBtn');
+ button.addEventListener('click',() => displayRandomCompliment(compliments));
 })();
     
